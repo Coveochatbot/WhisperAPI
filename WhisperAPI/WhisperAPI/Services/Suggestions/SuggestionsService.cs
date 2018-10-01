af4d7797b758aea6ea828339997445e1a5e8d3ad
Francis Lussier <daesolus6@gmail.com>
2018-10-01 14:12:06 -0400

    Add support for last suggestion question

--- WhisperAPI/WhisperAPI/Services/Suggestions/SuggestionsService.cs
+++ WhisperAPI/WhisperAPI/Services/Suggestions/SuggestionsService.cs
@@ -80,9 +80,11 @@ namespace WhisperAPI.Services.Suggestions
 
         public void UpdateContextWithNewQuestions(ConversationContext context, List<Question> questions)
         {
+            context.LastSuggestedQuestions.Clear();
             foreach (var question in questions)
             {
                 context.Questions.Add(question);
+                context.LastSuggestedQuestions.Add(question);
             }
         }
 
