be608eaf9b2eb907aee2a534bdbd7024701ac857
xahor <erdnacram15@gmail.com>
2018-10-27 11:44:30 -0400

    added default value (10) now all older tests passes

--- WhisperAPI/WhisperAPI/Models/Queries/Query.cs
+++ WhisperAPI/WhisperAPI/Models/Queries/Query.cs
@@ -12,10 +12,10 @@ namespace WhisperAPI.Models.Queries
 
         [Required(ErrorMessage = "MaxDocuments is required")]
         [JsonProperty("maxDocuments")]
-        public int MaxDocuments { get; set; }
+        public int MaxDocuments { get; set; } = 10;
 
         [Required(ErrorMessage = "maxQuestions is required")]
         [JsonProperty("maxQuestions")]
-        public int MaxQuestions { get; set; }
+        public int MaxQuestions { get; set; } = 10;
     }
 }
