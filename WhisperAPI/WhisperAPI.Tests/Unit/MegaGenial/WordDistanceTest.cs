d8604262fec6c6e20ae276d8fe18a500a0db00f5
Christopher <clemonde28@gmail.com>
2018-11-24 14:19:51 -0500

    Added tests for distance calculator

---
+++ WhisperAPI/WhisperAPI.Tests/Unit/MegaGenial/WordDistanceTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using WhisperAPI.Models.MegaGenial;
+
+namespace WhisperAPI.Tests.Unit.MegaGenial
+{
+    [TestFixture]
+    public class WordDistanceTest
+    {
+        [Test]
+        [TestCase]
+        public void WhenASpaceTypoIsMadeThenASmallDistanceIsReturned()
+        {
+            var word1 = "LeMot";
+            var word2 = "Le Mot";
+            var distance = DistanceCalculator.GetDistance(DistanceCalculator.ConvertWord(word1), DistanceCalculator.ConvertWord(word2));
+            Assert.AreEqual(1, distance);
+        }
+
+        [Test]
+        [TestCase]
+        public void WhenALetterTypoIsMadeThenASmallDistanceIsReturned()
+        {
+            var word1 = "LeMot";
+            var word2 = "LeMor";
+            var distance = DistanceCalculator.GetDistance(DistanceCalculator.ConvertWord(word1), DistanceCalculator.ConvertWord(word2));
+            Assert.AreEqual(1, distance);
+        }
+
+        [Test]
+        [TestCase]
+        public void WhenAnInversionTypoIsMadeThenASmallDistanceIsReturned()
+        {
+            var word1 = "LeMot";
+            var word2 = "LeMto";
+            var distance = DistanceCalculator.GetDistance(DistanceCalculator.ConvertWord(word1), DistanceCalculator.ConvertWord(word2));
+            Assert.AreEqual(1, distance);
+        }
+
+        [Test]
+        [TestCase]
+        public void WhenABigTypoIsMadeThenAMaxDistanceIsReturned()
+        {
+            var word1 = "LeMot";
+            var word2 = "LeMto43";
+            var distance = DistanceCalculator.GetDistance(DistanceCalculator.ConvertWord(word1), DistanceCalculator.ConvertWord(word2));
+            Assert.AreEqual(int.MaxValue, distance);
+        }
+
+        [Test]
+        [TestCase]
+        public void WhenNoTypeAreMadeThenANullDistanceIsReturned()
+        {
+            var word1 = "LeMot";
+            var word2 = "LeMot";
+            var distance = DistanceCalculator.GetDistance(DistanceCalculator.ConvertWord(word1), DistanceCalculator.ConvertWord(word2));
+            Assert.AreEqual(0, distance);
+        }
+    }
+}
